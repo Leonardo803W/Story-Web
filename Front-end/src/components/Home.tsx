af3b715282a408768b01ffeedef45b5a64ca52dc
Riganelli Leonardo <leoriganelli1@gmail.com>
2026-06-08 15:04:32 +0200

    adding content in home

--- Front-end/src/components/Home.tsx
+++ Front-end/src/components/Home.tsx
@@ -10,6 +10,8 @@ import imgScienza from "../img/scienza.jpg";
 import imgReligione from "../img/religione.jpg";
 import imgInvenzioni from "../img/invenzioni.jpg";
 import imgMisteri from "../img/misteri.webp";
+import imgPeste from "../img/peste.webp";
+import imgPeste2 from "../img/peste-removebg-preview.png";
 
 const epoche = [
     {
@@ -42,6 +44,73 @@ const epoche = [
       text: "Eta delle rivoluzioni, XIX secolo, XX secolo e XXI secolo",
       img: imgEtaContemporanea
     }
+]
+
+const TimeLine = [
+    {
+        id: 1,
+        title: "Medioevo",
+        content: [
+            {
+                id: 1,
+                title: "Europa",
+                text:"In Europa è stato un periodo di grandi cambiamenti, caratterizzato da guerre, carestie e pestilenze, ma anche da importanti sviluppi culturali e artistici.",
+                img: imgPeste,
+                img2: imgPeste2,
+            },
+            {
+                id: 2,
+                title: "Asia",
+                text: "L'asia visse un periodo d'oro, per esempio Cina sotto le dinastie Tang e Song furono perfezionati strumenti che cambiarono il mondo come la stampa a caratteri mobili, la polvere da sparo e la bussola magnetica."
+            },
+            {
+                id: 3,
+                title: "Americhe",
+                text: "In America civilta' avanzate come i Maya, svilupparono complessi sistemi di scrittura, astronomia e architettura monumentale."
+            },
+            {
+                id: 4,
+                title: "Africa",
+                text: "In Africa il nord divenne parte integrante del mondo islamico, influenzando profondamenti i commerci, nel mentre il sud Africa, nacquero e prosperarono gli imperi dell'oro."
+            },
+            {
+                id: 5,
+                title: "Oceania",
+                text: "In Oceania, a differenza degli altri continenti non vi furono grandi civilta, ma le popolazioni indigene svilupparono culture uniche e adattate ai loro ambienti e anche dimostrando straordinarie capacita' di navigazione."
+            },
+        ]
+    },
+    {
+        id: 2,
+        title: "Medioevo",
+        content: [
+            {
+                id: 1,
+                title: "Europa",
+                text:"In Europa è stato un periodo di grandi cambiamenti, caratterizzato da guerre, carestie e pestilenze, ma anche da importanti sviluppi culturali e artistici."
+            },
+            {
+                id: 2,
+                title: "Asia",
+                text: "L'asia visse un periodo d'oro, per esempio Cina sotto le dinastie Tang e Song furono perfezionati strumenti che cambiarono il mondo come la stampa a caratteri mobili, la polvere da sparo e la bussola magnetica."
+            },
+            {
+                id: 3,
+                title: "Americhe",
+                text: "In America civilta' avanzate come i Maya, svilupparono complessi sistemi di scrittura, astronomia e architettura monumentale."
+            },
+            {
+                id: 4,
+                title: "Africa",
+                text: "In Africa il nord divenne parte integrante del mondo islamico, influenzando profondamenti i commerci, nel mentre il sud Africa, nacquero e prosperarono gli imperi dell'oro."
+            },
+            {
+                id: 5,
+                title: "Oceania",
+                text: "In Oceania, a differenza degli altri continenti non vi furono grandi civilta, ma le popolazioni indigene svilupparono culture uniche e adattate ai loro ambienti e anche dimostrando straordinarie capacita' di navigazione."
+            },
+        ]
+    },
 ]
 
 const Home = () => {
@@ -134,6 +203,29 @@ const Home = () => {
               <h3>Misteri storici</h3>
               <p>La storia non e sempre chiara, ci sono misteri per cui non avremo mai risposte e altri a cui non avremo mai certezze</p>
             </div>
+          </div>
+        </section>
+
+        <section>
+          <div className="timelineCarousel">
+
+            {TimeLine.map((timeline) => (
+              <div key={timeline.id} className="timelineSlide">
+
+                <h3>
+                  Cosa succedeva nel mondo nel {timeline.title}?
+                </h3>
+
+                {timeline.content.map((item) => (
+                  <div key={item.id} className="carouselTimeLine-card">
+                    <h4>{item.title}</h4>
+                    <p>{item.text}</p>
+                  </div>
+                ))}
+
+              </div>
+            ))}
+
           </div>
         </section>
     </>
